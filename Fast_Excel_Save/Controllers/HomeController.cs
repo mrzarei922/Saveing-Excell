8724b2846b35db07af90351e85a705cfd8ccbbbd
mrzarei922 <118346903+mrzarei922@users.noreply.github.com>
2022-11-21 09:09:42 +0330

    Add files via upload

---
+++ Fast_Excel_Save/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Fast_Excel_Save.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fast_Excel_Save.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+      
+        public async Task<IActionResult> upload(IFormFile files)
+        {
+            if (files==null)
+            {
+                return Ok("plase select excell file!");
+            }
+            try
+            {
+                if (files.Length > 0)
+                {
+                    var filePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Upload", files.FileName));
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await files.CopyToAsync(stream);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                return Ok("false");
+            }
+           
+            var main = new Fast_Excel_Save_Lib.Main();
+           var test= main.Excel_Saving("./upload/"+files.FileName);
+            return Ok(test);
+        }
+    }
+}
---
+++ Fast_Excel_Save/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fast_Excel_Save.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
